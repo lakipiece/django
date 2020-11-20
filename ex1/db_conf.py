7549ac0fb6c25a0bba409899b59e07b8e128f640
root <root@ip-172-31-47-121.ap-northeast-2.compute.internal>
2020-11-20 03:30:25 +0000

    init Commit

---
+++ ex1/db_conf.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'django',                  
+        'USER': 'admin',                          
+        'PASSWORD': '1122moon',                  
+        'HOST': 'database-2.cstteuc7ccx3.ap-northeast-2.rds.amazonaws.com',                     
+        'PORT': '3306',                          
+    }
+}
